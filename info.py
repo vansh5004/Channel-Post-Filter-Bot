c93c2d6319fa9dbc59ac79b8fa2ab6ae217286c7
vansh5004 <145826508+vansh5004@users.noreply.github.com>
2023-11-04 13:31:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = 
-ADMIN        = 
-CHANNEL      = "@CyNiTeBaCkUp"
+API_ID       = 23565903
+API_HASH     = "2a87af806641e62069aac6fb658675d6"
+BOT_TOKEN    = "6589617401:AAGi6DdOvoUKHom7CgO6ngsyI1-G9tAlxLQ"
+SESSION      = "BQFPB4gAfwfbeM3IBRoZOEqosM2OWBUWE8ZZEBTb7gSesguBJozRY6wEdZdhPhhIsbXjflIdaVjXhpsy_tICyrCAAmxqT0tLRXfhMKI4CCeJP7bxTi6Vxo-_X5lHnADbhXhdVbo3zfVqBwHhbpMuCJBrOIzSrZ_Bx9E08Ekldg4fcEp3_uB-Ozjbfc_fP2POxXdBQRWVgAzy9PimWv436sc0KoScptWtnzVpwIa-wBAvnoneffsMhlqwVdCCLwmU-xeINIL2ZyuY5mgaP_K8AyfsAW_gCFs3iRfE8imNChBJmUBtCaf9K1ACYvudhvxpcQ0SNTyySlkPtjgMDC02jremq3-HFQAAAAB4ai0IAA"
+DATABASE_URI = "mongodb+srv://linksearchbot:linksearchbot@cluster0.0nlspf8.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1002095978146
+ADMIN        = 2020224264
+CHANNEL      = "@moviekorner_1"
